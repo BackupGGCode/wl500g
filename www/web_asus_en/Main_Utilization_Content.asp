BackupGGCode/wl500g

commit 38dbedf492f7fa8ce6456ea7b9c5e3eca4836930

www: syntax and style enhancements contributed by Anatoly Sotskov
git-svn-id: http://wl500g.googlecode.com/svn/trunk@2434 c3c545f6-d8d2-11dd-93da-dbb4cac96d7c

www/web_asus_en/Main_Utilization_Content.asp
@@ -1,3 +1,4 @@
+<html>
 <head>
 <title>ZVMODELVZ Web Manager</title>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
@@ -53,7 +54,7 @@
 	    wan_ifname="<% nvram_get_x("IPConnection","wan0_pppoe_ifname"); %>";
 	}
 	if (ipv6_proto=="tun6in4" || ipv6_proto=="tun6in4") {
-	    ipv6_ifname="six0";
+	    ipv6_ifname="sixtun";
 	}
 
 	process_element("wan", wan_ifname);
@@ -69,8 +70,6 @@
 <body onLoad="generate_page();" onunLoad="return unload_body();">
 <div id="overDiv" style="position:absolute; visibility:hidden; z-index:1000;"></div>
 <form method="GET" name="form" action="apply.cgi">
-<!-- Table for the conntent page -->
-<table width="666" border="0" cellpadding="0" cellspacing="0">
 <input type="hidden" name="current_page" value="Main_Utilization_Content.asp">
 <input type="hidden" name="next_page" value="Main_Utilization_Content.asp">
 <input type="hidden" name="next_host" value="">
@@ -80,8 +79,7 @@
 <input type="hidden" name="action_mode" value="">
 <input type="hidden" name="first_time" value="">
 <input type="hidden" name="action_script" value="">
-<tr>
-<td>
+<!-- Table for the conntent page -->
 <table width="666" border="1" cellpadding="0" cellspacing="0" bordercolor="#E0E0E0">
 <tr class="content_header_tr">
     <td class="content_header_td_title" colspan="2">Status &amp; Log - System Utilization</td>
@@ -99,10 +97,7 @@
 	</select></td>
 </tr>
 </table>
-</td>
-</tr>
-<tr>
-<td>
+
 <table width="666" border="1" cellpadding="0" cellspacing="0" bordercolor="#E0E0E0">
 <tr class="content_section_header_tr">
     <td class="content_section_header_td" colspan="2">System Load Monitoring</td>
@@ -159,24 +154,18 @@
     </td></tr></table></td>
 </tr>
 </table>
-</td>
-</tr>
-</form>
-<tr>
-<td>
+
 <table width="666" border="1" cellpadding="0" cellspacing="0" bordercolor="#B0B0B0">
-<tr bgcolor="#CCCCCC"><td colspan="3"><font face="arial" size="2"><b>&nbsp;</b></font></td></tr>
+<tr bgcolor="#CCCCCC"><td colspan="3"><font face="arial" size="2">&nbsp;</font></td></tr>
 <tr bgcolor="#FFFFFF">  
-    <td height="25" width="34%"></td>
-    <td height="25" width="33%"></td>
-    <td height="25" width="33%">
+    <td height="25" width="71%"></td>
+    <td height="25" >
 	<input type="hidden" name="action_mode" value=" Refresh ">
 	<div align="center"><font face="Arial"> <input class=inputSubmit onMouseOut="buttonOut(this)" onMouseOver="buttonOver(this)" onClick="document.form3.next_host.value = location.host; onSubmitCtrl(this, ' Refresh ')" type="submit" value="Refresh" name="action"></font></div> 
 	</td>
 </tr>
 </table>
-</td>
-</tr>
-</table>
+
 </form>
 </body>
+</html>

commit 44a2e3e45964627c784e951193e75d90788bab77

[RT-N] Merged revisions 2469 via svnmerge from /trunk r2469 | theMIROn | 2011-01-13 01:27:49 +0500   www: fixed ipv6 graph, was broken by r2434

www/web_asus_en/Main_Utilization_Content.asp
@@ -53,8 +53,8 @@
 	if (wan_proto=="usbmodem") {
 	    wan_ifname="<% nvram_get_x("IPConnection","wan0_pppoe_ifname"); %>";
 	}
-	if (ipv6_proto=="tun6in4" || ipv6_proto=="tun6in4") {
-	    ipv6_ifname="sixtun";
+	if (ipv6_proto=="tun6in4" || ipv6_proto=="tun6to4") {
+	    ipv6_ifname="six0";
 	}
 
 	process_element("wan", wan_ifname);